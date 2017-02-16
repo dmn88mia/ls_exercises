accda0136d498c66b02fbabccad277b50f2ebf54
David Navarro <dmn88mia@gmail.com>
2017-02-15 22:08:38 -0500

    Finished exercise 101

---
+++ 101_109_small_problems/easy_3/searching_101.rb
@@ -0,0 +1,88 @@
+# Understand the problem : 
+# print even numbers from 1 to 99
+
+# Set up your test cases
+
+# Describe your inputs, outputs, and data structures
+
+# Describe your algorithm
+# Create a loop that pushes the the 6 numbers into an array. After gathering the
+# 6 numbers I will remove and save the last number in the array and with that
+# use include? to see if the number was mentioned in the first 5.
+
+# Even Numbers
+
+# Print the even numbers from 1 to 99, inclusive. All numbers should be printed on separate lines.
+
+# -------------------------------------------------------------------------------------
+# Begin coding:
+
+
+
+def num_appear?(array)
+  num = array.pop
+  array.include?(num) ? puts("The number #{num} appears in #{array}.") : puts("The number #{num} does not appear in #{array}.")
+end
+
+list = %w(1st 2nd 3rd 4th 5th last).map do |str|
+  puts "Enter the #{str} number:"
+  answer = ''
+  loop do
+    answer = gets.chomp
+    break if answer.to_i.to_s == answer
+    puts "That is not a number, please enter a number:"
+  end
+  answer = answer.to_i
+end
+
+num_appear?(list)
+
+# Or ---------------------------------------------------------------------------------------------------------------------------
+
+def num_appear?(array)
+  num = array.pop
+  array.include?(num) ? puts("The number #{num} appears in #{array}.") : puts("The number #{num} does not appear in #{array}.")
+end
+
+list = []
+
+%w(1st 2nd 3rd 4th 5th last).each_with_index do |str|
+  puts "Enter the #{str} number:"
+  answer = ''
+  loop do
+    answer = gets.chomp
+    break if answer.to_i.to_s == answer
+    puts "That is not a number, please enter a number:"
+  end
+  answer = answer.to_i
+  list << answer
+end
+
+num_appear?(list)
+
+# # # Or ---------------------------------------------------------------------------------------------------------------------------
+
+def num_appear?(array)
+  num = array.pop
+  array.include?(num) ? puts("The number #{num} appears in #{array}.") : puts("The number #{num} does not appear in #{array}.")
+end
+
+list = []
+sequence = ['1st', '2nd', '3rd', '4th', '5th', 'last']
+counter = 0
+
+loop do
+  break if list.size == 6
+  puts "Enter the #{sequence[counter]} number:"
+  answer = ''
+  loop do
+    answer = gets.chomp
+    break if answer.to_i.to_s == answer
+    puts "That is not a number, please enter a number:"
+  end
+  answer = answer.to_i
+  list << answer
+  counter += 1
+end
+
+num_appear?(list)

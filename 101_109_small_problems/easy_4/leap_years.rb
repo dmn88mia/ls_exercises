0e77c551184d8754de1ada109f3ade1b77d0a938
David Navarro <dmn88mia@gmail.com>
2017-02-22 22:50:31 -0500

    Finished leap years

---
+++ 101_109_small_problems/easy_4/leap_years.rb
@@ -0,0 +1,67 @@
+# Understand the problem : 
+# return true or false if the year is a leap year.
+
+# Set up your test cases
+# leap_year?(2016) == true
+# leap_year?(2015) == false
+# leap_year?(2100) == false
+# leap_year?(2400) == true
+# leap_year?(240000) == true
+# leap_year?(240001) == false
+# leap_year?(2000) == true
+# leap_year?(1900) == false
+# leap_year?(1752) == true
+# leap_year?(1700) == false
+# leap_year?(1) == false
+# leap_year?(100) == false
+# leap_year?(400) == true
+
+# Describe your inputs, outputs, and data structures
+# input will be a year
+# the return value will be a boolean value
+
+# Describe your algorithm
+# if and else statements
+
+# Leap Years (Part 1)
+
+# In the modern era under the Gregorian Calendar, leap years occur in every year that is evenly divisible by 4, unless the year is also divisible by 100. If the year is evenly divisible by 100, then it is not a leap year unless the year is evenly divisible by 400.
+
+# Assume this rule is good for any year greater than year 0. Write a method that takes any year greater than 0 as input, and returns true if the year is a leap year, or false if it is not a leap year.
+
+# leap_year?(2016) == true
+# leap_year?(2015) == false
+# leap_year?(2100) == false
+# leap_year?(2400) == true
+# leap_year?(240000) == true
+# leap_year?(240001) == false
+# leap_year?(2000) == true
+# leap_year?(1900) == false
+# leap_year?(1752) == true
+# leap_year?(1700) == false
+# leap_year?(1) == false
+# leap_year?(100) == false
+# leap_year?(400) == true
+
+# -------------------------------------------------------------------------------------
+# Begin coding:
+
+
+def leap_year?(year)
+  return true if year % 4 == 0 && !(year % 100 == 0)
+  year % 100 == 0 && year % 400 == 0 ? true : false
+end
+
+p leap_year?(2016) #== true
+p leap_year?(2015) #== false
+p leap_year?(2100) #== false
+p leap_year?(2400) #== true
+p leap_year?(240000) #== true
+p leap_year?(240001) #== false
+p leap_year?(2000) #== true
+p leap_year?(1900) #== false
+p leap_year?(1752) #== true
+p leap_year?(1700) #== false
+p leap_year?(1) #== false
+p leap_year?(100) #== false
+p leap_year?(400) #== true

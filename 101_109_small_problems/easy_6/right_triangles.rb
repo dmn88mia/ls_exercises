5f8f2249df358f9fa2ebe437ac0485b3c39ec343
David Navarro <dmn88mia@gmail.com>
2017-03-30 20:36:49 -0400

    saved

--- 101_109_small_problems/easy_6/right_triangles.rb
+++ 101_109_small_problems/easy_6/right_triangles.rb
@@ -55,35 +55,89 @@
 #   end
 # end
 
-def triangle(num, angle)
-  list = ['right', 'left', 'upside down right', 'upside down left']
-  if list.include?(angle) == false
-    puts "#{angle} is not one of the listed angels"
-    puts "Please choose one of the following: #{list}"  
-    return nil
-  end
+# def triangle(num, angle)
+#   list = ['right', 'left', 'upside down right', 'upside down left']
+#   if list.include?(angle) == false
+#     puts "#{angle} is not one of the listed angels"
+#     puts "Please choose one of the following: #{list}"  
+#     return nil
+#   end
 
-  star_count = num - (num - 1)
-  spaces_count = num - 1
+#   star_count = num - (num - 1)
+#   spaces_count = num - 1
 
-  if angle == 'right' || angle == 'left'
-    num.times do 
-      angle == 'right' ? puts((' ' * spaces_count) + ('*' * star_count)) : puts(('*' * star_count) + (' ' * spaces_count))
-      spaces_count -= 1
-      star_count += 1
-    end
-  else
-    star_count = num
-    spaces_count = 0
+#   if angle == 'right' || angle == 'left'
+#     num.times do 
+#       angle == 'right' ? puts((' ' * spaces_count) + ('*' * star_count)) : puts(('*' * star_count) + (' ' * spaces_count))
+#       spaces_count -= 1
+#       star_count += 1
+#     end
+#   else
+#     star_count = num
+#     spaces_count = 0
 
-    num.times do 
-     angle == 'upside down right' ? puts((' ' * (spaces_count)) + ('*' * star_count)) : puts(('*' * star_count) + (' ' * (spaces_count)))
-     spaces_count += 1
-     star_count -= 1
-    end
+#     num.times do 
+#      angle == 'upside down right' ? puts((' ' * (spaces_count)) + ('*' * star_count)) : puts(('*' * star_count) + (' ' * (spaces_count)))
+#      spaces_count += 1
+#      star_count -= 1
+#     end
+#   end
+# end
+# # Please enter, num size and for position enter : 'right', 'left', 'upside down right' or 'upside down left'
+# triangle(5, 'upside down left')
+
+# ---------------==================------------
+# Question 1
+
+# In this hash of people and their age,
+
+# Starting with this string:
+
+
+numbers = ['2', '3', '5', '7', '8', '11', '13', '15', '18']
+odd_numbers = []
+
+numbers.each do |number|
+  number = number.to_i
+  odd_numbers << number if number.odd?
+end
+
+p odd_numbers
+
+numbers = ['2', '3', '5', '7', '8', '11', '13', '15', '18']
+odd_numbers1 = []
+counter = 0
+
+loop do
+  counter += 1
+  number = numbers[counter].to_i
+
+  odd_numbers1 << number unless number.even?
+
+  break if counter == numbers.size
+end
+
+p odd_numbers1
+
+numbers = ['2', '3', '5', '7', '8', '11', '13', '15', '18']
+odd_numbers2 = []
+
+for number in numbers
+  if number.to_i.odd?
+    odd_numbers2.push(number)
   end
 end
-# Please enter, num size and for position enter : 'right', 'left', 'upside down right' or 'upside down left'
-triangle(5, 'upside down left')
 
+p odd_numbers2
 
+numbers = ['2', '3', '5', '7', '8', '11', '13', '15', '18']
+odd_numbers3 = []
+number = 0
+
+until number == numbers.size
+  odd_numbers3 << numbers[number] if number.to_i.odd?
+
+  number += 1
+end
+
+p odd_numbers3
